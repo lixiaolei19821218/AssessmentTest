e2ad4b759217241b8654df36aa4b75ed13082f8d
lixiaolei19821218 <735534185@qq.com>
2015-06-10 17:33:34 +0800

    Add

---
+++ AssessmentTest/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Liquid.AssessmentLibrary;
+
+namespace AssessmentTest
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataSource data = new DataSource();
+            DataSource.DataCallback cb = new DataSource.DataCallback(Callback);
+            data.SubscribeWithWPFSynchronization(cb, this);
+        }
+
+        private void Callback(FinancialData data)
+        {
+            DataContext = data;
+            //data.PricingSpecRows.
+        }
+    }
+}
---
+++ AssessmentTest/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AssessmentTest
+{
+    class MainWindowViewModel
+    {
+        public string PricingSpecName { get; set; }
+        public IEnumerable<string> PricingSpecs { get; set; }
+        public DateTime TimeStamp { get; set; }
+        //public IEnumerable<> PricingSpecRows
+    }
+}
